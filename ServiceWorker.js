6b22fdf549e6fe5960ede31a00cc25a0edccf347
kalielson <kalielson@hotmail.com>
2025-01-16 16:38:11 -0300

    Update ServiceWorker.js

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -31,3 +31,29 @@ self.addEventListener('fetch', function (e) {
       return response;
     })());
 });
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).then((response) => {
+      return response;
+    }).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
