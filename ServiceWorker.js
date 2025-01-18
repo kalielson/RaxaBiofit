30fe67739251fe62afbb5330f7532c8a82a97c72
kalielson <kalielson@hotmail.com>
2025-01-18 17:34:34 -0300

    Add files via upload

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -14,7 +14,7 @@ self.addEventListener('install', function (e) {
     e.waitUntil((async function () {
       const cache = await caches.open(cacheName);
       console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
+      //await cache.addAll(contentToCache);
     })());
 });
 
